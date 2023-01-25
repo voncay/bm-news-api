82304ecf7b46f67475444a92c89cc136fd3c3358
Marion Pierret <marion.pierretsu@gmail.com>
2023-01-25 14:56:03 +0100

    add delete put routes

--- routes/articlesRouter.js
+++ routes/articlesRouter.js
@@ -10,10 +10,33 @@ articlesRouter.get('/articles', (req, res) => {
     .catch(err => res.json(err))
 })
 
-articlesRouter.post('/articles', (req, res) => {
+articlesRouter.post('/articles/', (req, res) => {
     Articles
     .create(req.body)
     .then(newArticle => res.json(newArticle))
+    .catch(err => res.json(err))
+})
+
+articlesRouter.get('/articles/:id', (req, res) =>{
+    Articles
+    .findOne({_id : req.params.id})
+    .then(oneArticle => res.json(oneArticle))
+    .catch(err => res.json(err))
+})
+
+articlesRouter.put('/articles/:id', async (req, res) => {
+    await Articles.findOne({_id : req.params.id})
+    await Articles.updateOne({$set : req.body})
+    await Articles.findOne({_id : req.params.id})
+    .then(newArticle => res.json(newArticle))
+    .catch(err => res.json(err))
+})
+
+
+articlesRouter.delete('/articles/:id', (req, res) => {
+    Articles
+    .deleteOne({_id : req.params.id})
+    .then(() => res.json('Article deleted sucessfully'))
     .catch(err => res.json(err))
 })
 
